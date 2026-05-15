b1f82f8d3bb5b689e22b94e98331a537899d25e9
kgolaganopoosa <kgolaganopeterpoo@gmail.com>
2026-05-15 12:26:27 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,7 @@
-const CACHE_NAME = 'petal-cache-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
-
-// Install Event
+const CACHE_NAME = 'petal-v1';
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['/'])));
 });
-
-// Fetch Event (Required for PWA)
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
