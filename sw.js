e64c4f3c64ed7deb525add1d09239b4e87517b2a
kgolaganopoosa <kgolaganopeterpoo@gmail.com>
2026-05-15 12:20:07 +0200

    Add service worker for asset caching
    Implement service worker for caching assets.

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'petal-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install Event
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch Event (Required for PWA)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
